713811f1d64dd53a4663d899613d3c684387a565
SarahWambui <sarahwambui843@gmail.com>
2025-11-18 09:20:09 +0300

    feat: :bug: debug login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,18 +25,9 @@ class LoginController extends Controller
 
         $remember = $request->has('remember');
 
-        // Dump input credentials (for debugging, never do this in public!)
-        dd([
-            'email' => $request->email,
-            'password' => $request->password,
-        ]);
-
-
         if (Auth::attempt($request->only('email', 'password'), $remember)) {
 
             $request->session()->regenerate();
-
-            dd(Auth::user());
 
             $user = Auth::user();
 
